e0ee33c3825fd36bad75860c22e5b6e85a926b09
Adriano Henrique Cantão <adriano.cantao@gmail.com>
2019-09-30 15:49:55 -0300

    Add files via upload

--- plotting_datasets.R
+++ plotting_datasets.R
@@ -1,11 +1,8 @@
-#require(caret)
 require(mlbench)
 require(KODAMA)
 require(gridExtra)
-require(grid)
 
-datasetNames_2d <- c("mlbench.spirals(n=
-                     examples, cycles=2, sd=0.05)",
+datasetNames_2d <- c("mlbench.spirals(n=examples, cycles=2, sd=0.05)",
                      "mlbench.cassini(n=examples)",
                      "mlbench.shapes(n=examples)",
                      "mlbench.smiley(n=examples)",
@@ -34,32 +31,27 @@ datasetNames_3d <- c("mlbench.cuboids(n=examples)",
 #WORKING WITH 2D DATASETS
 plot2D <- function(datasetNames_2d, examples){
   par(mfrow=c(3,4))
-  #p_2d <- list()
   for(datasetName in datasetNames_2d){
     #setFilename <- gsub("\\([\\(\\)\\w\\d\\s\\=\\.,]*\\)", "", datasetName, perl=TRUE, ignore.case=TRUE)
     setFilename <- sub("\\(.*", "", datasetName) # * before '('
-    
     lib_name <- sub("\\..*", "", setFilename) # * before '.'
     func_name <- sub(".*\\.", "", setFilename)# * after '.'
     
     if (lib_name == 'mlbench'){
       dataset <- eval( parse( text=datasetName ) )
-      #p_2d[[setFilename]] <- print(plot(dataset, main=setFilename))
       plot(dataset, main=setFilename)
     }
     else if (lib_name == 'KODAMA'){
       #remove 'kodama' from name
       datasetName <- sub(".*KODAMA.", "", datasetName) 
-      #add code to create class
+      #add code to create the class feature (target/label)
       datasetName <- paste0("cbind(",datasetName,", rep(1:2, each=round(examples/2)))")
       
       #evaluate the string as a function
       dataset <- eval( parse( text=datasetName ) )
-      #change the col names to 0,1,'CLASS' #may be removed.
-      colnames(dataset) <- c(0:1, "CLASS")
-      #saving plot to a list
-      #p_2d[[setFilename]] <- plot(dataset, main=setFilename, col=dataset[,3])
-      plot(dataset, main=setFilename, col=dataset[,3])
+      #renaming columns to 0,1,'class' 
+      colnames(dataset) <- c(0:1, "class")
+      plot(dataset, main=setFilename, col=dataset[,'class'])
     }
     else{
       print('error in library name.')
@@ -67,14 +59,12 @@ plot2D <- function(datasetNames_2d, examples){
   }
 }
 
-
 #WORKING WITH 3D DATASETS
 plot3D <- function(datasetNames_3d, examples){
   p <- list()
   for(datasetName in datasetNames_3d){
     #setFilename <- gsub("\\([\\(\\)\\w\\d\\s\\=\\.,]*\\)", "", datasetName, perl=TRUE, ignore.case=TRUE)
     setFilename <- sub("\\(.*", "", datasetName) # * before '('
-    
     lib_name <- sub("\\..*", "", setFilename) # * before '.'
     func_name <- sub(".*\\.", "", setFilename)# * after '.'
     
@@ -87,17 +77,17 @@ plot3D <- function(datasetNames_3d, examples){
       datasetName <- sub(".*KODAMA.", "", datasetName) 
       #add code to create class
       datasetName <- paste0("cbind(",datasetName,", rep(1:3, each=100))")
-      
       #evaluate the string as a function
       dataset <- eval( parse( text=datasetName ) )
-      #change the col names to 0,1,'CLASS' #may be removed.
-      colnames(dataset) <- c("x.1", "x.2", "x.3", "classes")
+      #renaming columns to 0,1,'class' 
+      colnames(dataset) <- c("x.1", "x.2", "x.3", "class")
       #saving plot to a list
-      #p_2d[[setFilename]] <- plot(dataset, main=setFilename, col=dataset[,3])
-      p[[setFilename]] <- cloud(x.3~x.1+x.2, groups=classes, data=as.data.frame(dataset), main=setFilename)
+      p[[setFilename]] <- cloud(x.3~x.1+x.2, groups=class, data=as.data.frame(dataset), main=setFilename)
+    }
+    else{
+      print('error in library name.')
     }
   }
-  #grid.arrange(grobs=p, ncol=3, top = textGrob("3D datasets"))
   grid.arrange(grobs=p, ncol=4)
 }
 
